b3fe9fe45d2f0f96b7ba5e0e7866ef18da9035cd
Paweł Czarnecki <biznes.pczarnecki@gmail.com>
2024-02-26 15:47:55 +0100

    add graphical project author credit

--- components/layout/Footer/FooterBottom/FooterBottom.js
+++ components/layout/Footer/FooterBottom/FooterBottom.js
@@ -10,8 +10,11 @@ const FooterBottom = ({ items }) => {
         <>
             <div className="border-t border-t-gray-400">
                 <div className="container">
-                    <div className="flex justify-center items-center py-9">
+                    <div className="flex flex-col justify-center items-center py-9">
                         <p className="text-gray-700 text-sm">© {curYear} {items?.footerBottomText || ''}</p>
+                        <p className="text-gray-700 text-sm mt-4">
+                            Graphical design by <a href="https://www.figma.com/@pro_designer" target="_blank" rel="noopener noreferrer nofollow">@pro_designer</a> (Figma community)
+                        </p>
                     </div>
                 </div>
             </div>
